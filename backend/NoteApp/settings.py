e681a05371d4dd6ab699420dfb39a08072e65878
spencer-vs <isaacharu17@gmail.com>
2026-05-01 11:22:02 +0100

    Building Forgot Password Page 0.4

--- backend/NoteApp/settings.py
+++ backend/NoteApp/settings.py
@@ -101,9 +101,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173"
 ]
 
-# CORS_ALLOW_HEADERS = [
-#     "authori"
-# ]
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+
+]
 
 ROOT_URLCONF = "NoteApp.urls"
 
