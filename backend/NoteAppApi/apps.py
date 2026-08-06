789585d60a8491ae6fe3c8a9c420ad7062501fcd
spencer-vs <isaacharu17@gmail.com>
2026-08-06 19:54:27 +0100

    Implementing search tutorials 0.2

--- backend/NoteAppApi/apps.py
+++ backend/NoteAppApi/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
 
-class NoteappapiConfig(AppConfig):
+class NoteAppApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "NoteAppApi"
+
+    def ready(self):
+        import NoteAppApi.signals
