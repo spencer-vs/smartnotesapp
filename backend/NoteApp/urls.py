721586fedf32402f4860a0f10afcc365dc29b76e
spencer-vs <isaacharu17@gmail.com>
2026-09-21 19:53:55 +0100

    Add Android App Links assetlinks endpoint

--- backend/NoteApp/urls.py
+++ backend/NoteApp/urls.py
@@ -18,9 +18,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.http import JsonResponse
+
+
+def assetlinks(request):
+    return JsonResponse([
+        {
+            "relation": [
+                "delegate_permission/common.handle_all_urls"
+            ],
+            "target": {
+                "namespace": "android_app",
+                "package_name": "com.anonymous.SmartNotesMobile",
+                "sha256_cert_fingerprints": [
+                    "FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C"
+                ]
+            }
+        }
+    ], safe=False)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("NoteAppApi.urls")),
     path("api/auth/", include("NoteAppCore.urls")),
+    path(
+        ".well-known/assetlinks.json",
+        assetlinks,
+        name="assetlinks"
+    ),
 ]
